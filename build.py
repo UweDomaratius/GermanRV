bf00f23246771606337e0af7aa00365e400ae6ca
UweDomaratius <106884507+UweDomaratius@users.noreply.github.com>
2025-05-24 21:21:14 +0200

    feat: initial code takeover (#4)
    Added all graphics and code from the previous location.
    No feature changes compared to last manually released version.

---
+++ build.py
@@ -0,0 +1,68 @@
+import shutil
+import subprocess
+import glob
+import os
+import sys
+from pathlib import Path
+from datetime import datetime
+from constants import constants
+
+inputs = [
+    "src/german_rvs.pnml",
+    "src/recolour.pnml",
+]
+
+buses = sorted(glob.glob("src/bus/*.pnml"))
+inputs.extend(buses)
+trams = sorted(glob.glob("src/tram/*.pnml"))
+inputs.extend(trams)
+trucks = sorted(glob.glob("src/truck/*.pnml"))
+inputs.extend(trucks)
+
+# clean the build dir, we'll recreate all files
+shutil.rmtree('build',ignore_errors=True)
+Path("build").mkdir()
+
+# create custom tags including version + time/date
+# on github the version number is passed in via command line, 
+# if no such argument is given, a placeholder is used
+now = datetime.now()
+dt = now.strftime("%Y-%m-%d %H:%M:%S")
+version = "local build " + dt
+if len(sys.argv) > 1:
+    version = sys.argv[1]
+version = "VERSION:"+version
+
+with open('custom_tags.txt', 'w') as file:
+  file.write(version)
+  file.write("\nTITLE: German Road Vehicles")
+
+# create the actual nml file by concatenating all inputs
+with open('build/german_rvs.nml','wb') as output:
+    for f in inputs:
+        with open (f, 'rb') as fd:
+            shutil.copyfileobj(fd, output)
+            output.write(b"\n")
+            
+
+# replace the defined constants
+# Read in the file
+with open('build/german_rvs.nml', 'r') as file:
+  filedata = file.read()
+
+# Replace the target string
+for key,value in constants.items():
+    filedata = filedata.replace(key, value)
+
+# Write the file out again
+with open('build/german_rvs.nml', 'w') as file:
+  file.write(filedata)
+  
+# call nmlc to create the grf output
+subprocess.run(["nmlc", "-c", "--grf", "build/german_rvs.grf","build/german_rvs.nml"])
+
+#shutil.copyfile('custom_tags.orig', 'custom_tags.txt')
+dt = now.strftime("%Y-%m-%d_%H-%M-%S")
+grfdate="build/german_rvs_"+dt+".grf"
+shutil.copyfile('build/german_rvs.grf', grfdate)
+os.remove("custom_tags.txt")
--- release.config.cjs
+++ release.config.cjs
@@ -4,8 +4,14 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-	'@semantic-release/commit-analyzer', 
-	'@semantic-release/release-notes-generator',
-	'@semantic-release/github'
+	"@semantic-release/commit-analyzer",
+	"@semantic-release/release-notes-generator",
+	["@semantic-release/github",
+	  {
+	    "assets": [
+		  { "path": "german_rvs.zip", "label": "German Road Vehicle set" }
+		]
+	  }
+	]
   ]
 };
