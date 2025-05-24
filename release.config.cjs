1d709e0ac635db9a77448c0683a9c82f9621fe5e
Uwe <uwe@domaratius.de>
2025-05-24 18:13:00 +0200

    simplify config

--- release.config.cjs
+++ release.config.cjs
@@ -6,10 +6,6 @@ module.exports = {
   plugins: [
 	'@semantic-release/commit-analyzer', 
 	'@semantic-release/release-notes-generator',
-	['@semantic-release/github',
-	  {
-        "assets": ["changelog.md"]
-	  }
-    ]
+	'@semantic-release/github'
   ]
 };
